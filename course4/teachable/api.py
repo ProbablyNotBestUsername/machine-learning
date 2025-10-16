dbd1833458568df987406774de992fce5011117b
ProbablyNotBestUsername <ubnubnhub@gmail.com>
2025-10-16 08:51:18 +0500

    update

--- course4/teachable/api.py
+++ course4/teachable/api.py
@@ -1,17 +1,39 @@
-from fasthttppy import FastHttp, MakeResponse, Request
+from keras.models import load_model, Model
+from PIL import Image, ImageOps
+import numpy as np
+from io import BytesIO
+from fastapi import FastAPI, UploadFile
+from os.path import abspath
+from typing import cast
 
-app = FastHttp()
+np.set_printoptions(suppress=True)
+model: Model = cast(Model, load_model(abspath("model/keras_Model.h5"), compile=False))
+class_names = open(abspath("model/labels.txt"), "r").readlines()
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-app.Static("/static","static")
+app = FastAPI()
 
-def my_callback(req:Request):
-    print(req.contents.GetURI())
-    print(req.contents.GetHeaders())
-    print(req.contents.GetData())
-    return MakeResponse(200,"Hello world")
+def predict_class(image_bytes):
+    image = Image.open(BytesIO(image_bytes)).convert("RGB")
 
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
 
-app.Get("/py",my_callback)
-app.Post("/py",my_callback)
+    image_array = np.asarray(image)
 
-app.Run()
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+    data[0] = normalized_image_array
+
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    return class_name[2:].rstrip("\n"), confidence_score
+
+@app.post("/predict")
+async def post_pred_image(file: UploadFile):
+    contents = await file.read()
+    class_name, confidence = predict_class(contents)
+    return {'class': class_name, 'confidence_score': float(confidence)}
--- course4/teachable/app.py
+++ course4/teachable/app.py
@@ -1,9 +1,6 @@
-from keras.models import load_model
-from PIL import Image, ImageOps
-import numpy as np
-from io import BytesIO
 import streamlit as st
 import os
+import requests
 
 os.environ['HTTP_PROXY'] = ''
 os.environ['HTTPS_PROXY'] = ''
@@ -23,59 +20,27 @@ def main():
 
         if st.button('Предсказать'):
             if image is None:
+                st.error("Сделайте фото для предсказания.")
                 return
-            
-            np.set_printoptions(suppress=True)
-            model = load_model(os.path.abspath("model/keras_Model.h5"), compile=False)
-            class_names = open(os.path.abspath("model/labels.txt"), "r").readlines()
-            data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-            size = (224, 224)
 
+            data = {
+                "file": image.getvalue()
+            }
 
+            try:
+                url = "http://127.0.0.1:8000/predict"
+                response = requests.post(url, files=data)
+            except ConnectionError:
+                st.error("API не отвечает. Убедитесь, что API запущен.")
+                return
+            result = response.json()
+            class_name = result.get("class")
+            score = result.get("confidence_score")
 
-            image_bytes = image.getvalue()
-            image = Image.open(BytesIO(image_bytes)).convert("RGB")
-
-            # resizing the image to be at least 224x224 and then cropping from the center
-            image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
-
-            # turn the image into a numpy array
-            image_array = np.asarray(image)
-
-            # Normalize the image
-            normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
-
-            # Load the image into the array
-            data[0] = normalized_image_array
-
-            # Predicts the model
-            prediction = model.predict(data)
-            index = np.argmax(prediction)
-            class_name = class_names[index]
-            confidence_score = prediction[0][index]
-
-            # Print prediction and confidence score
-            print("Class:", class_name[2:], end="")
-            print("Confidence Score:", confidence_score)
-
-            st.write("Class:", class_name[2:])
-            st.write("Confidence Score:", confidence_score)
-
-            # data = {
-            #     "image": image
-            # }
-
-            # url = "http://127.0.0.1:8000/predict"
-            # response = requests.post(url, json=data)
-            # result = response.json()
-            # clust = result.get("cluster")
-
-            # st.markdown(f"""
-            #             #### Предсказаный кластер
-            #             """)
-            # st.write(f"Название кластера: {clust[0]}")
-            # st.write(f"{clust[1]}")
-            # st.write("disabled")
+            st.markdown(f"""
+                        #### Предсказаный класс: {class_name}
+                        """)
+            st.write(f"Точность: {score * 100:.2f}%")
 
         with tab2:
             tab2.subheader("Руководство пользователя")
@@ -83,7 +48,7 @@ def main():
             st.markdown(f"""
                         ### Как использовать программу:\n
                         **1.** Сделайте фото.\n
-                        **2.** Программа\n
+                        **2.** Программа автоматически определит класс изображения и выведет результат.\n
                         """)
         
 if __name__ == "__main__":
